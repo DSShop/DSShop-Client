1f0891c8c18cf803d68b7d562949a62364e7e57f
Lollio64 <141979356+Lollio64@users.noreply.github.com>
2023-09-18 19:18:24 +0200

    Added non-compilable graphics code.

--- source/graphics.cpp
+++ source/graphics.cpp
@@ -1,7 +1,76 @@
+#include <assert.h>
+#include <string.h>
 #include <stdio.h>
-#include <citro2D.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define TOP_SCREEN_HEIGHT 240
-#define TOP_SCREEN_WIDTH 400
-#define BOTTOM_SCREEN_HEIGHT 320
-#define BOTTOM_SCREEN_WIDTH 240
+#include <3ds.h>
+#include <citro2d.h>
+
+#define MAX_SPRITES   50;
+#define SCREEN_WIDTH  360;
+#define SCREEN_HEIGHT 240;
+
+typedef struct {
+	C2D_Sprite spr;
+	float dx, dy; 
+} Sprite;
+
+static C2D_SpriteSheet spriteSheet;
+
+static void initSprites() {
+
+        Sprite* sprite = &sprite[1];
+
+		// Sets the image and position
+		C2D_SpriteFromSheet(&sprite->spr, spriteSheet, 1);
+		C2D_SpriteSetPos(&sprite->spr, 0, 0);
+}
+
+int main(int argc, char* argv[]) {
+
+	romfsInit();
+	gfxInitDefault();
+	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+	C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+	C2D_Prepare();
+	consoleInit(GFX_BOTTOM, NULL);
+
+	// Create screens
+	C3D_RenderTarget* bottom = C2D_CreateScreenTarget(GFX_BOTTOM, GFX_LEFT);
+
+	// Load graphics
+	spriteSheet = C2D_SpriteSheetLoad("romfs:/gfx/sprites.t3x");
+	if (!spriteSheet) svcBreak(USERBREAK_PANIC);
+
+	// Initialize sprites
+	initSprites();
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		hidScanInput();
+
+		// Respond to user input
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_START)
+			break; // break in order to return to hbmenu
+
+		// Render the scene
+		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C2D_TargetClear(bottom, C2D_Color32f(0.0f, 0.0f, 0.0f, 1.0f));
+		C2D_SceneBegin(bottom);
+		C2D_DrawSprite(&sprite[1].spr);
+		C3D_FrameEnd(0);
+	}
+
+	// Delete graphics
+	C2D_SpriteSheetFree(spriteSheet);
+
+	// Deinit libs
+	C2D_Fini();
+	C3D_Fini();
+	gfxExit();
+	romfsExit();
+	return 0;
+}
